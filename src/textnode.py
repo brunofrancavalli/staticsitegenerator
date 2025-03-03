62c4d91c8a2be6883e6ebb616734812b1cecb313
Bruno de Franca Valli <bruno.valli@bnsf.com>
2025-03-03 09:06:42 -0600

    Adding new tests

--- src/textnode.py
+++ src/textnode.py
@@ -1,7 +1,6 @@
 from enum import Enum
 
 class TextType(Enum):
-<<<<<<< HEAD
     BOLD = "**"
     ITALIC = "_"
     CODE = "`"
@@ -10,27 +9,12 @@ class TextType(Enum):
 
 class TextNode:
     def __init__(self, text, text_type, url = None):
-=======
-    BOLD_TEXT = "**"
-    ITALIC_TEXT = "_"
-    CODE_TEXT = "`"
-    LINK_FORMAT = "[]()"
-    IMAGE_FORMAT = "![]()"
-
-class TextNode:
-    def __init__(self, text, text_type, url):
->>>>>>> c8fe633c41a00511a05e53283d0b83570f25273e
         self.text = text
         self.text_type = text_type
         self.url = url
     
-<<<<<<< HEAD
     def __eq__(self, other):
         return True
-=======
-    def __eq__():
-        return TRUE
->>>>>>> c8fe633c41a00511a05e53283d0b83570f25273e
     
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
