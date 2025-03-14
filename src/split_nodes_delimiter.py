e15fdccb7209ddc4ef8a6c93f0e37b0109badc66
CQAkI9M3D%8q <bruno.valli@bnsf.com>
2025-03-14 08:29:51 -0500

    fixd issue with starting with a delimiter

--- src/split_nodes_delimiter.py
+++ src/split_nodes_delimiter.py
@@ -6,10 +6,13 @@ def split_nodes_delimiter(old_node_list, delimiter, text_type):
         processing_type = False
         new_nodes = []
         for old_nodes_item in old_nodes_list:
-            if processing_type:
-                new_nodes.append(NodeText(old_nodes_item, text_type))
-            else:
-                new_nodes.append(NodeText(old_nodes_item, TextType.TEXT))
+            # only processs if we have something to process
+            if len(old_nodes_item) > 0:
+                if processing_type:
+                    new_nodes.append(NodeText(old_nodes_item, text_type))
+                else:
+                    new_nodes.append(NodeText(old_nodes_item, TextType.TEXT))
+            # always flip the flag since we are starting with false we flip and we should only be within the entries
             processing_type = not processing_type
     
     return new_nodes
--- src/test_split_nodes_delimiter.py
+++ src/test_split_nodes_delimiter.py
@@ -7,8 +7,27 @@ class TestSplitNodesDelimiter(unittest.TestCase):
     def test_default_code(self):
         node = NodeText("This is text with a `code block` word", TextType.TEXT)
         new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
-
         self.assertListEqual( new_nodes,[NodeText("This is text with a ", TextType.TEXT),NodeText("code block", TextType.CODE),NodeText(" word", TextType.TEXT)])
+
+    def test_default_bold(self):
+        node = NodeText("This is text with a **code block** word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertListEqual( new_nodes,[NodeText("This is text with a ", TextType.TEXT),NodeText("code block", TextType.BOLD),NodeText(" word", TextType.TEXT)])
+
+    def test_default_italic(self):
+        node = NodeText("This is text with a _code block_ word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "_", TextType.ITALIC)
+        self.assertListEqual( new_nodes,[NodeText("This is text with a ", TextType.TEXT),NodeText("code block", TextType.ITALIC),NodeText(" word", TextType.TEXT)])
+    
+    def test_default_code_atstart(self):
+        node = NodeText("`code block` word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertListEqual( new_nodes,[NodeText("code block", TextType.CODE),NodeText(" word", TextType.TEXT)])
+
+    def test_default_code_atend(self):
+        node = NodeText("This is text with a `code block`", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertListEqual( new_nodes,[NodeText("This is text with a ", TextType.TEXT),NodeText("code block", TextType.CODE)])
 
 if __name__ == "__main__":
     unittest.main()
