16c25cec976acbfcaa6bf42e7dca28ce4abd56fa
CQAkI9M3D%8q <bruno.valli@bnsf.com>
2025-03-18 08:12:57 -0500

    adding more tests for extract_markdown_images

--- src/test_extract_markdown_images.py
+++ src/test_extract_markdown_images.py
@@ -3,11 +3,21 @@ import unittest
 from extract_markdown_images import extract_markdown_images
 
 class test_extract_markdown_images(unittest.TestCase):
-    def test_url_rickroll(text):
+    def test_url_rickroll_2entries(self):
         text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
         result = extract_markdown_images(text)
         self.assertEqual(result,[("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")])
 
+    def test_url_rickroll_1entry(self):
+        text = "This is text with a https://i.imgur.com/aKaOqIh.gif and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        result = extract_markdown_images(text)
+        self.assertEqual(result, [("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")])
+
+    def test_url_rickroll_0entry(self):
+        text = "This is text with a https://i.imgur.com/aKaOqIh.gif and obi wan)"
+        result = extract_markdown_images(text)
+        self.assertEqual(result, [])
+    
     
 if __name__ == "__main__":
     unittest.main()
